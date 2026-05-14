5f27a61ffa3f3b0c814d0f5e04d6ecca28339713
jazzynpro <reader_jaz@hotmail.com>
2026-05-14 16:41:25 -0500

    PARTE 1 SIMULADOR

--- simulador_completo.js
+++ simulador_completo.js
@@ -1,4 +1,4 @@
-  let clientes = [];
+  let clientesArreglo = []; //Arreglo
   let creditos = [];
 
   let tasaInteres = 15;
@@ -34,3 +34,82 @@ function guardarTasa(){
     mostrarTexto("mensajeTasa", "La tasa debe estar entre 10% y 20%")
   }
 }
+
+function guardarCliente(){
+  let cedula = recuperaraTexto("txtCedula");
+  let nombre = recuperaraTexto("txtNombre");
+  let apellido = recuperaraTexto("txtApellido");
+  let ingresos = recuperarFloat("txtIngresos");
+  let egresos = recuperarFloat("txtEgresos");
+
+  let cliente ={};
+ 
+  cliente.cedula = cedula;
+  cliente.nombre = nombre;
+  cliente.apellido = apellido;
+  cliente.ingresos = ingresos;
+  cliente.egresos = egresos;
+ 
+  let busqueda = buscarCliente(cedula);
+ 
+  if(busqueda == null){
+    clientesArreglo.push(cliente);    // guarda el cliente dentro de clientesArreglo[]
+    pintarClientes();                 //llama a la funcion pintar para mostrar datos en la tabla  
+  } else{
+    busqueda.nombre = nombre;
+    busqueda.apellido = apellido;
+    busqueda.ingresos = ingresos;
+    busqueda.egresos = egresos;
+    //clientesArreglo.push(busqueda);
+    pintarClientes();    
+  }
+ 
+  limpiar();
+   
+}
+
+function pintarClientes(){
+  let tabla = document.getElementById("tablaClientes");
+  tabla.innerHTML= "";
+
+  let elementosTabla;
+  let filaTabla = "";
+
+  for(i=0; i<clientesArreglo.length; i++){ //usamos el contador que llega desde 0 hasta el final del arreglo
+  elementosTabla = clientesArreglo[i]; //obtenemos el cliente en la posicion i
+
+  filaTabla += "<tr>"+
+              "<td>"+elementosTabla.cedula + "</td>"+
+              "<td>"+elementosTabla.nombre + "</td>"+
+              "<td>"+elementosTabla.apellido + "</td>"+
+              "<td>"+elementosTabla.ingresos + "</td>"+
+              "<td>"+elementosTabla.egresos + "</td>"+
+              "<td><button onclick='seleccionarCliente("+ elementosTabla.cedula +")'>Actualizar</button>"+"<button>"+'Eliminar'+"</button></td>"+      
+              "</tr>";      
+  }
+  tabla.innerHTML = filaTabla;
+}
+function buscarCliente(cedula){
+  let elementoTabla;
+  let clienteEncontrado = null;
+ 
+  for(let i=0; i<clientesArreglo.length; i++){
+    elementoTabla = clientesArreglo[i];
+      if(elementoTabla.cedula == cedula){
+        clienteEncontrado = elementoTabla;
+        break;                    //encuentra el valor y suspende la ejecucion del for
+      }
+  }
+ 
+  return clienteEncontrado;
+}
+
+function limpiar(){
+    document.getElementById("txtCedula").value = "";
+    document.getElementById("txtNombre").value = "";
+    document.getElementById("txtApellido").value = "";
+    document.getElementById("txtIngresos").value = "";
+    document.getElementById("txtEgresos").value = "";
+}
+ 
+ 
